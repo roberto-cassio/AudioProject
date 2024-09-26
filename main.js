5fbc036ec111eee4cde719ddb0f4b267017ef2ef
roberto-cassio <rcslino99@gmail.com>
2024-09-26 13:49:04 -0300

    added canvas line graph

--- main.js
+++ main.js
@@ -7,6 +7,61 @@ navigator.mediaDevices.getUserMedia({ audio: true })
     source.connect(analyser);
     var bufferLength = analyser.frequencyBinCount;
     var dataArray = new Uint8Array(bufferLength);
+    var lastValidFrequency = 0;
+    var canvas = document.getElementById('audioCanvas');
+    if (canvas == null) {
+        console.error("Canvas recebeu null");
+    }
+    else {
+        var canvasCtx_1 = canvas.getContext('2d');
+        var lineHistory_1 = [];
+        function draw() {
+            requestAnimationFrame(draw);
+            analyser.getByteFrequencyData(dataArray);
+            var sampleRate = audioCtx.sampleRate;
+            var maxIndexByteFrequency = 0;
+            for (var i = 1; i < bufferLength; i++) {
+                if (dataArray[i] > dataArray[maxIndexByteFrequency]) {
+                    maxIndexByteFrequency = i;
+                }
+            }
+            var frequencyInHz = maxIndexByteFrequency * (sampleRate / 2) / bufferLength;
+            var validThreshold = 500;
+            if (Math.abs(frequencyInHz - lastValidFrequency) < validThreshold) {
+                lineHistory_1.push(frequencyInHz);
+                lastValidFrequency = frequencyInHz;
+            }
+            if (lineHistory_1.length > canvas.width) {
+                lineHistory_1.shift();
+            }
+            canvasCtx_1.fillStyle = 'rgb(0, 0, 0)';
+            canvasCtx_1.fillRect(0, 0, canvas.width, canvas.height);
+            canvasCtx_1.beginPath();
+            canvasCtx_1.lineWidth = 2;
+            canvasCtx_1.strokeStyle = 'rgb(255, 255, 255)';
+            canvasCtx_1.moveTo(0, canvas.height - (lineHistory_1[0] / 1000 * canvas.height));
+            for (var i = 1; i < lineHistory_1.length; i++) {
+                var y = canvas.height - (lineHistory_1[i] / 1000 * canvas.height);
+                canvasCtx_1.lineTo(i, y);
+            }
+            canvasCtx_1.stroke();
+            /* const barWidth = (canvas.width / bufferLength) * 2.5;
+             let barHeight;
+             let x = 0;
+ 
+             for (let i = 0; i < bufferLength; i++) {
+                 barHeight = dataArray[i];
+         
+                 canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+                 canvasCtx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
+                 x += barWidth +1;
+                 */
+            canvasCtx_1.fillStyle = 'rgb(255, 255, 255)';
+            canvasCtx_1.font = '20px Arial';
+            canvasCtx_1.fillText("Frequ\u00EAncia: ".concat(Math.round(frequencyInHz), " Hz"), 10, 30);
+        }
+        draw();
+    }
 })
     .catch(function (err) {
     console.error("Falha ao Acessar o Microfone");
--- main.ts
+++ main.ts
@@ -10,6 +10,68 @@ navigator.mediaDevices.getUserMedia({audio:true})
     const bufferLength = analyser.frequencyBinCount;
     const dataArray = new Uint8Array(bufferLength);
 
+    let lastValidFrequency = 0;
+    const canvas =<HTMLCanvasElement> document.getElementById('audioCanvas');
+    if(canvas == null){
+        console.error("Canvas recebeu null")
+    }else{
+        const canvasCtx =<CanvasRenderingContext2D> canvas.getContext('2d');
+        let lineHistory: number[] = [];
+        function draw (){
+            requestAnimationFrame(draw);
+    
+            analyser.getByteFrequencyData(dataArray);
+            const sampleRate = audioCtx.sampleRate;
+    
+            let maxIndexByteFrequency = 0;
+            for (let i = 1; i<bufferLength; i++){
+                if (dataArray[i] > dataArray[maxIndexByteFrequency]){
+                    maxIndexByteFrequency = i;
+                }
+            }
+    
+            const frequencyInHz = maxIndexByteFrequency * (sampleRate/2)/bufferLength; 
+
+            const validThreshold = 500;
+            if (Math.abs(frequencyInHz - lastValidFrequency) < validThreshold){
+                lineHistory.push(frequencyInHz);
+                lastValidFrequency = frequencyInHz;
+            }
+            if (lineHistory.length > canvas.width){
+                lineHistory.shift();
+            }
+            
+            canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+            canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+            canvasCtx.beginPath();
+            canvasCtx.lineWidth = 2;
+            canvasCtx.strokeStyle = 'rgb(255, 255, 255)';
+            canvasCtx.moveTo(0, canvas.height - (lineHistory[0] / 1000 * canvas.height));
+
+            for (let i = 1; i < lineHistory.length; i++){
+                const y = canvas.height - (lineHistory[i] / 1000 * canvas.height);
+                canvasCtx.lineTo(i,y);
+            }
+
+            canvasCtx.stroke();
+           /* const barWidth = (canvas.width / bufferLength) * 2.5;
+            let barHeight;
+            let x = 0;
+
+            for (let i = 0; i < bufferLength; i++) {
+                barHeight = dataArray[i];
+        
+                canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+                canvasCtx.fillRect(x, canvas.height - barHeight / 2, barWidth, barHeight / 2);
+                x += barWidth +1;
+                */
+                canvasCtx.fillStyle = 'rgb(255, 255, 255)';
+                canvasCtx.font = '20px Arial';
+                canvasCtx.fillText(`Frequência: ${Math.round(frequencyInHz)} Hz`, 10, 30); 
+    }  
+    draw();
+}
 })
 .catch(function(err){
     console.error("Falha ao Acessar o Microfone")
